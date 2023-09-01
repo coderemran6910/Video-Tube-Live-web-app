79fde42862cacce23d7db06b0d40e894209906b2
Coder Emran <coderemran6910@gmail.com>
2023-09-02 00:07:45 +0600

    solved some mistake

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const loadCategories = async() => {
     // console.log(categories);
 
     const categoriesContainer = document.getElementById('categories');
-    categoriesTab.forEach((categoryTab, index) => {
+    categoriesTab.forEach((categoryTab) => {
        
         // console.log(categoryTab);
         const span = document.createElement('span');
@@ -20,7 +20,7 @@ const loadCategories = async() => {
 const loadPostById =async(id) =>{
 try {
 
-    // console.log(id);
+
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const data = await response.json();
     const allCategoriesData = data.data;
@@ -43,8 +43,8 @@ try {
         })
        renderCard(sortedData); 
      }
-    sortByView.addEventListener('click', sortedData);
-    sortByViewSM.addEventListener('click', sortedData);
+    sortByView.addEventListener('click', sortedData); //desktop button
+    sortByViewSM.addEventListener('click', sortedData);//mobile button
 
 
     
@@ -76,20 +76,56 @@ try {
 
 
         withOutSortData.forEach(category => {
+            const card = document.createElement('div'); 
 
             // sec to hour:min:sec
             const seconds = category?.others?.posted_date;
             const hour = Math.floor(seconds / 3600);
             const minites = Math.floor((seconds % 3600) / 60);
             const actualDate = `${hour}hrs ${minites}min ago`;
-            // console.log(actualDate);
+            const emptyDate = "";
 
-            const card = document.createElement('div'); 
+           if (seconds === "") {
+               card.innerHTML = `
+               <div id="card" class="card card-compact bg-base-100 shadow-xl w-80">
+               <figure class="w-80 h-[200px]">
+                   <img class="w-full h-full" src="${category.thumbnail}" />
+               </figure>
+               <div class="card-body">
+       
+                   <div class="flex gap-4 items-center">
+                       <div class="avatar">
+                       <div class="w-10 rounded-full">
+                         <img src="${category?.authors[0]?.profile_picture}" />
+                       </div>
+                     </div>
+       
+       
+                       <div>
+                           <h2 class="font-bold leading-6"> ${category?.title} </h2>
+                       </div>
+                   </div>
+       
+       
+           <div class="flex gap-4">
+               <div class="font-sm font-normal">${category?.authors[0]?.profile_name}</div>
+               <div class="font-sm font-normal">
+               <img class="w-5 h-5"  src="${category?.authors[0]?.verified === true ? '/project resources/verified.png' : '/project resources/white.png'}">
+               </div>
+           </div>
+       
+                   <p> <span>${category?.others.views}</span> views</p>
+               </div>
+           </div>
+               
+               `
+            
+           }else{
             card.innerHTML=`
             <div id="card" class="card card-compact bg-base-100 shadow-xl w-80">
             <figure class="w-80 h-[200px] relative">
                 <img class="w-full h-full" src="${category.thumbnail}" />
-                <div class="absolute bottom-3 right-3 p-3 text-white bg-black px-4 py-2 rounded-md text-xs font-normal"> ${actualDate? actualDate : ""} </div>
+                <div class="absolute bottom-3 right-3 p-3 text-white bg-black px-4 py-2 rounded-md text-xs font-normal"> ${actualDate? actualDate : emptyDate} </div>
             </figure>
             <div class="card-body">
     
@@ -110,7 +146,7 @@ try {
         <div class="flex gap-4">
             <div class="font-sm font-normal">${category?.authors[0]?.profile_name}</div>
             <div class="font-sm font-normal">
-            <img class="w-5 h-5"  src="${category?.authors[0]?.verified === true ? '/project resources/verified.png' : '/project resources/white.jpg'}">
+            <img class="w-5 h-5"  src="${category?.authors[0]?.verified === true ? '/project resources/verified.png' : '/project resources/white.png'}">
             </div>
         </div>
     
@@ -118,6 +154,8 @@ try {
             </div>
         </div>
             `
+           }
+
             categoriesCardContainer.appendChild(card);
         })
     }
@@ -136,11 +174,3 @@ loadPostById('1000')
 
 // category call function
 loadCategories()
-
-
-const totalSeconds = 1672656000;
-const hours = Math.floor(totalSeconds / 3600);
-const remainingSeconds = totalSeconds % 3600;
-const minutes = Math.floor(remainingSeconds / 60);
-
-console.log(`Hours: ${hours}, Minutes: ${minutes}`);
